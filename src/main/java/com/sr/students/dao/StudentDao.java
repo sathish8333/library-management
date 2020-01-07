70bdc9bc521b5288d86bcf2d21ff54f888c5fd43
sathish reddy <sathishreddy.sr123@gmail.com>
2020-01-07 23:24:58 +0530

    adding configuration files

--- src/main/java/com/sr/students/dao/StudentDao.java
+++ src/main/java/com/sr/students/dao/StudentDao.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import com.sr.students.pojo.Student;
 
@@ -16,8 +17,23 @@ public class StudentDao {
 		Session session = factory.getCurrentSession();
 		Transaction trans = session.beginTransaction();
 		session.save(stuobj);
-		//trans.commit();
-		
+		// trans.commit();
+
+		session.close();
+		factory.close();
+
+	}
+
+	public static void getStudent(int id) {
+		factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+		Transaction trans = session.beginTransaction();
+		Query query = session.createQuery("from student where id=:stu_id ");
+		query.setParameter("stu_id", id);
+		query.list();
+		// trans.commit();
+
 		session.close();
 		factory.close();
 
